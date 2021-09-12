c3bd4907dbb3d9047f4af57a607054b4365b32c5
josheMoreno <jose_antonio.moreno@uao.edo.co>
2021-09-11 20:16:37 -0500

    Creacion de la pantalla de login

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,7 +9,27 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+
+  public materias: Array<string> =[
+    "Fisica 1",
+    "Fisica 2",
+    "Matematicas"
+  ];
+
+  public opciones: Array<any> = [
+    {opt:"Perfil",uri:"/home/monitor"},
+    {opt:"Área personal",uri:"AreaPersonal"},
+    {opt:"Gestión tutorias",uri:"GestionTutoria"},
+    {opt:"Metricas",uri:"Metricas"},
+    {opt:"Cerrar sesión",uri:"CerrarSesion"}
+  ]
+
   ngOnInit(): void {
+    console.log(this.materias);
+  }
+
+  check():boolean{
+    return false;
   }
 
 }
