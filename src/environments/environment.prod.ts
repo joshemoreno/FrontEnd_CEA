7cec252a3a63ace8ec7aa25f007ba3fc667fd107
josheMoreno <jose_antonio.moreno@uao.edo.co>
2021-09-25 15:48:50 -0500

    ajuste de variables de entorno para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+
+  urlBack: 'http://192.168.20.22',
+  portBack: '3000',
+  
+  // EndPoints
+  loginEndpoint: '/api/auth/login',
+  logonEndpoint: '/api/auth/logon',
 };
