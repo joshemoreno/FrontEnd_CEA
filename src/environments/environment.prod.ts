1972e383f8dbca4005c1f0d8441d2726450e3717
josheMoreno <jose_antonio.moreno@uao.edo.co>
2021-11-11 23:43:03 -0500

    ajuste en variables de entorno pro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,9 +11,30 @@ export const environment = {
   production: true,
 
   urlBack: 'https://back-cea.herokuapp.com',
+
+  //WebEx
+  uriGetCode: 'https://webexapis.com/v1/authorize?client_id=Ce070a17e97544e67bb329f030e4ff59426af5ea618a17a715f1bb13d5a7fd3a7&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2FwebEx&scope=spark%3Akms%20meeting%3Aschedules_write&state=set_state_here',
+  uriAccessToken:'https://webexapis.com/v1/access_token',
+  uriCreateMeeting:'https://webexapis.com/v1/meetings',
+  uriMeetingInvitees:'https://webexapis.com/v1/meetingInvitees',
+  redirect_uri:'http://localhost:4200/webEx',
+  grant_type:'authorization_code',
+  client_secret:'8b3799187636aaa9dd6a556c40b56e5f380f3914ebe5a0bb6111b002c0d3a370',
+  client_id:'Ce070a17e97544e67bb329f030e4ff59426af5ea618a17a715f1bb13d5a7fd3a7',
+    
   
   // EndPoints
   loginEndpoint: '/api/auth/login',
   logonEndpoint: '/api/auth/logon',
   subjectsEndpoint: '/api/subject',
+
+
+  firebaseConfig:{
+    apiKey: "AIzaSyAAsBO_6-J0yooOHmKvhHWa8jQgJhbFSLs",
+    authDomain: "proyectocea.firebaseapp.com",
+    projectId: "proyectocea",
+    storageBucket: "proyectocea.appspot.com",
+    messagingSenderId: "17826992490",
+    appId: "1:17826992490:web:2a5b954a72ff490dc61513"
+  }
 };
